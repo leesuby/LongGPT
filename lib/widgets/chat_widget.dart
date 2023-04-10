82d96f5bde1a953579346110032d1e809f94f8e6
leesuby <62803507+leesuby@users.noreply.github.com>
2023-04-10 21:53:07 +0700

    remove like icon

--- lib/widgets/chat_widget.dart
+++ lib/widgets/chat_widget.dart
@@ -66,25 +66,6 @@ class ChatWidget extends StatelessWidget {
                                   fontSize: 16),
                             ),
                 ),
-                chatIndex == 0
-                    ? const SizedBox.shrink()
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Icon(
-                            Icons.thumb_up_alt_outlined,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Icon(
-                            Icons.thumb_down_alt_outlined,
-                            color: Colors.white,
-                          )
-                        ],
-                      ),
               ],
             ),
           ),
